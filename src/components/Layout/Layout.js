8b3d246d0f7a84a9c3a15b169f512b1805549529
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-20 14:27:03 -0300

    Aplicação de Estilos no Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/HOC';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
   <Aux>
   <div>Toolbar, SideDrawer, Backdrop</div>
-  <main>
+  <main className={classes.Conteudo}>
     {props.children}
   </main>
   </Aux>
