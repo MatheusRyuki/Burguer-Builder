98703678990499872f5fc83cd01386ea8887d308
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-20 11:47:11 -0300

    Estrutura encapsulada do Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Aux from '../../hoc/HOC';
 
 const layout = ( props ) => (
+  <Aux>
   <div>Toolbar, SideDrawer, Backdrop</div>
   <main>
     {props.children}
   </main>
+  </Aux>
 );
+
+
+export default layout;
