ab656f5794e0cc6bc543a493c8fdbf2a03a53090
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-20 11:48:43 -0300

    Layout no app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-      
+        <Layout>
+        <p> Teste </p>
+        </Layout>
       </div>
     );
   }
