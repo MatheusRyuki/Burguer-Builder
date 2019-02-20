962092eb6144d8c8283f2ee28e1ece731892e9b4
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-20 14:22:33 -0300

    Estrutura do Container finalizado

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import Aux from '../../hoc/Aux';
 
 class BurguerBuilder extends Component {
   render () {
     return (
-      
+      <Aux>
+        <div>Burguer</div>
+        <div>Build Controls</div>
+      </Aux>
     );
   }
 }
+
+export default BurguerBuilder;
