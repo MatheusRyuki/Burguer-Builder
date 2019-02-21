c0497b63208ded3d4ac266802ebbab8e6f1ad005
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 14:13:57 -0300

    Aplicação do Estilo no Individual

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './BuildControl.css';
 
 const buildControl = (props) => {
-  <div>
-    <div>{props.label}</div>
-    <button>Menos</button>
-    <button>Mais</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Menos</button>
+    <button className={classes.More}>Mais</button>
   </div>
 };
 
