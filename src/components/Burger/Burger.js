1effd2a59e2d5e9a7f3c0f074926b633f67bbd06
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 11:13:13 -0300

    Adição componente do Burger com Ingredientes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.css';
 
 const burger = (props) => {
     return (
-      <div>
-      
+      <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top"/>
+        <BurgerIngredient type="cheese"/>
+        <BurgerIngredient type="meat"/>
+        <BurgerIngredient type="bread-bottom"/>
       </div>
     );
 };
