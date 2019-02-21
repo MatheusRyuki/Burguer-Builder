2739e3ac01732da31d4f634e0d0392c9550a44c1
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 14:31:41 -0300

    Funções de controle de ingredientes e preço

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/HOC';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 1.5,
+  cheese: 1.2,
+  meat: 3.9,
+  bacon: 2.1
+};
+
 class BurguerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,14 +17,33 @@ class BurguerBuilder extends Component {
       bacon: 1,
       cheese: 2,
       meat: 2
-    }
+    },
+    totalPrice: 12
   };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients =  {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice +  priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+  }
+
+  removeIngredientHandler = (type) => {
+
+  }
 
   render () {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler }/>
       </Aux>
     );
   }
