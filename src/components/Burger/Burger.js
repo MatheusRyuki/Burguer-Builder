be1d4a838332ff625426126ce6c0965b24e837d6
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 13:56:13 -0300

    Reduzir os arrays só pra um

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,7 +8,10 @@ const burger = (props) => {
         return [...Array(props.ingredients[igKey])].map((_, i ) => {
           return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
 
     return (
       <div className={classes.Burger}>
