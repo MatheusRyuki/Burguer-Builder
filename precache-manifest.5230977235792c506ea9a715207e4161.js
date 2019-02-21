461d9161493378b57bda9894e560c97392256616
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 15:13:25 -0300

    Updates

---
+++ precache-manifest.5230977235792c506ea9a715207e4161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8ba32fd0f9c5dc5bbe4",
+    "url": "/Burguer-Builder/static/js/runtime~main.c8ba32fd.js"
+  },
+  {
+    "revision": "640f70b7df05f028d076",
+    "url": "/Burguer-Builder/static/js/main.640f70b7.chunk.js"
+  },
+  {
+    "revision": "c9f66c5e9416898e74d4",
+    "url": "/Burguer-Builder/static/js/2.c9f66c5e.chunk.js"
+  },
+  {
+    "revision": "640f70b7df05f028d076",
+    "url": "/Burguer-Builder/static/css/main.3038e117.chunk.css"
+  },
+  {
+    "revision": "2a30811b30aca826112c824062d3cb9c",
+    "url": "/Burguer-Builder/index.html"
+  }
+];
