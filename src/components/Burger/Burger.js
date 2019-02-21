0295fbf0f573ac46614ea4d3faff721c46564633
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 13:57:27 -0300

    Aviso de não tiver nenhum ingrediente

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i ) => {
           return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -12,7 +12,9 @@ const burger = (props) => {
     .reduce((arr, el) => {
       return arr.concat(el)
     }, []);
-
+    if (transformedIngredients.length === 0 ) {
+        transformedIngredients = <p>Por favor, coloque alguns ingredientes!</p>
+    }
     return (
       <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
