eab440af9c80939f7563d2be8ea9e7da616fd1cd
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-21 14:20:31 -0300

    Build Control Renderização

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'Cheese'},
+    { label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => {
-  <div>
-
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label} label={ctrl.label}/>
+    ))}
   </div>
 };
 
