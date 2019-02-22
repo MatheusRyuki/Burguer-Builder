20562c68b15cdd3703e7181685f8b01448e1d9e9
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-22 09:35:36 -0300

    HOTFIX

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,10 +4,6 @@ import classes from './Layout.css';
 
 const layout = ( props ) => (
   <Aux>
-<<<<<<< HEAD
-=======
-  //Toolbar, SideDrawer, Backdrop
->>>>>>> ff6b2070bc50d9ced4b2385ea22383750ef0ed71
   <main className={classes.Conteudo}>
     {props.children}
   </main>
