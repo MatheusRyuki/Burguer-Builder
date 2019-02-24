b13c4a7b63c641aaed64b08dd0cabdf72def5b85
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 19:49:12 -0300

    hotfix-v1.5

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import Logo from '../Logo/Logo';
+import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
-import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/HOC';
+import Backdrop from '../UI/Backdrop/Backdrop';
+import Aux from '../../hoc/HOC';
 
 const sideDrawer = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
@@ -22,7 +22,7 @@ const sideDrawer = (props) => {
           <NavigationItems />
         </nav>
       </div>
-    <Aux/>
+    </Aux>
   );
 };
 
