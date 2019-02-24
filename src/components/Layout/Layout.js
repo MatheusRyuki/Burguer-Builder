5c46a0d161b5ae20aac5bc53bd2ebc95be93f7b8
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 19:09:58 -0300

    Menu responsivo finalizado

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,10 +13,16 @@ class Layout extends Component {
     this.setState({showSideDrawer: false});
   }
 
+  sideDrawerToggleHandler = () => {
+    this.setState( ( prevState ) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    });
+  }
+
   render () {
     return (
       <Aux>
-        <Toolbar/>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer
          open={this.state.showSideDrawer}
          closed={this.sideDrawerClosedHandler}/>
---
+++ src/components/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const drawerToggle = (props) => (
+  <div onClick={props.clicked}>
+    MENU
+  </div>
+);
+
+export default drawerToggle;
