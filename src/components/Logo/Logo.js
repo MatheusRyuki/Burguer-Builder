0faeda80371125c50fb9fbfeadf19ca612339a3b
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 09:47:04 -0300

    Estilização da Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.css';
 
 const logo = (props) => (
-  <div>
+  <div className={classes.Logo}>
     <img src={burgerLogo} alt='MyBurger'/>
   </div>
 );
