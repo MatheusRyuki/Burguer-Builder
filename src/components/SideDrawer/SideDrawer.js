932d83bacbc404c236b96e6d964d65f7c6880136
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 16:27:48 -0300

    Ajustes responsivos

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ const sideDrawer = (props) => {
 
   return (
       <div className={classes.SideDrawer}>
-        <Logo/>
+        <div className={classes.Logo}>
+         <Logo/>
+        </div>
         <nav>
           <NavigationItems />
         </nav>
