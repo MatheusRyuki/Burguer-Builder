00a66c426535db724024001bfeb351cf71b853be
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 15:21:22 -0300

    Aplicação dos itens de navegação da Toolbar

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,12 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem/>
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/">
+      Checkout
+    </NavigationItem>
   </ul>
 );
 
