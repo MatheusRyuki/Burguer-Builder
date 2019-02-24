eec9f48316dc096300e6bf999faf392498f49b2f
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 09:43:02 -0300

    Criação do Componente da Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+const logo = (props) => (
+  <div>
+    <img src={burgerLogo} alt='MyBurger'/>
+  </div>
+);
+
+export default logo;
