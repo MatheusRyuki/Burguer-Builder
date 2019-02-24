64eec65b2fd20c9b4c7597d3a3f4220ca7de95a1
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 15:58:05 -0300

    SideDrawer implementado

---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+
+  return (
+      <div className={classes.SideDrawer}>
+        <Logo/>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+  );
+};
+
+
+export default sideDrawer;
