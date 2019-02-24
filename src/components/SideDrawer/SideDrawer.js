d1ecb1aabcb2ca3e9cc26178d093465ce5ed4f28
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 16:36:55 -0300

    Visualização condicional do Toolbar

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -10,7 +10,7 @@ const sideDrawer = (props) => {
         <div className={classes.Logo}>
          <Logo/>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
           <NavigationItems />
         </nav>
       </div>
