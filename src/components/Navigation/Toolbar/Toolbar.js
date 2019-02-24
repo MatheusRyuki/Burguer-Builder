abb75b052b28b44ed80141f2fb3d119e3b66da15
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-24 09:18:15 -0300

    Criado componente Toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const toolbar = (props) => (
+  <header>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>
+      ...
+    </nav>
+  </header>
+);
+
+export default toolbar;
