ade924df611a2d1f455021851c9adc7d383b2b09
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-03 08:54:13 -0300

    Indo e voltando navegação

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,22 @@ class Checkout extends Component {
       bacon: 1
     }
   }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}/>
       </div>
     )
   }
