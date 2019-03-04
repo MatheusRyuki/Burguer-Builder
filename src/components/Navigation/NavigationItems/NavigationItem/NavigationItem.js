eee430baedcd8adf97feacc268fd8626135389ac
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-04 06:54:57 -0300

    Implementado links de navegação

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import { NavLink } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link}
-      className={props.active ? classes.active : null}>{props.children}</a>
+    <NavLink to={props.link}
+      exact={props.exact}
+      activeClassName={classes.active}>{props.children}</NavLink>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,11 +4,11 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>
+    <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="/">
-      Checkout
+    <NavigationItem link="/orders">
+      Orders
     </NavigationItem>
   </ul>
 );
