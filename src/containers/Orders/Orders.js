f70b3e7f3ccb8566d1d520ad0d93957e376a9758
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-04 07:02:44 -0300

    Pegando os pedidos no servidor

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state ={
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+
+        this.setSyate({loading: false, orders: fetchedOrders});
+      })
+      .catch(err => {
+        this.setSyate({loading: false});
+      });
+  }
+
   render () {
     return (
       <div>
@@ -11,4 +36,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
