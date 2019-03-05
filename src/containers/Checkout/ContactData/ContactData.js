29514347e079fae220097257900cf5bd971fb86c
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-05 17:53:17 -0300

    Formulário Dinâmico

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,12 +85,23 @@ class ContactData extends Component {
   }
 
   render () {
+      const formElementsArray = [];
+      for (let key in this.state.orderForm) {
+        formElementsArray.push({
+          id: key,
+          config: this.state.orderForm[key],
+
+        });
+      }
       let form = (
         <form>
-          <Input elementType="" elementConfig="" value="" />
-          <Input elementType="" elementConfig="" value=""  />
-          <Input elementType="" elementConfig="" value=""  />
-          <Input elementType="" elementConfig="" value=""  />
+          {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value} />
+            ))}
           <Button btnType="Success">PEDIR</Button>
         </form>
       );
