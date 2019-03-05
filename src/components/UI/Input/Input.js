1bc6084357adc2841b67464e595fcb0e1a254b0a
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-05 18:13:32 -0300

    Lidando com o Input do Usuário

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,11 +9,17 @@ const input = (props) => {
         inputElement = <input  className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
         break;
     case ('textarea'):
-      inputElement = <textarea  className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+      inputElement = <textarea  className={classes.InputElement}
+        {...props.elementConfig} 
+        value={props.value}
+        onChange={props.changed}/>;
       break;
     case ('select'):
       inputElement = (
-        <select  className={classes.InputElement} value={props.value}>
+        <select
+          onChange={props.changed}
+          className={classes.InputElement}
+          value={props.value}>
         {props.elementConfig.options.map(option => {
           <option key={option.value} value={option.value}>
             {option.displayValue}
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,6 +84,18 @@ class ContactData extends Component {
       });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier];
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
   render () {
       const formElementsArray = [];
       for (let key in this.state.orderForm) {
@@ -100,7 +112,8 @@ class ContactData extends Component {
               key={formElement.id}
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
-              value={formElement.config.value} />
+              value={formElement.config.value}
+              changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
             ))}
           <Button btnType="Success">PEDIR</Button>
         </form>
