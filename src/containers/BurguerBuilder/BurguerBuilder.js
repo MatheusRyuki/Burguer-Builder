4f0705d2d9f7a047dd2a8970150b79020b1600f8
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-09 10:50:26 -0300

    Hotfix

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import actionsType from '../../store/actions';
+import * as actionsType from '../../store/actions';
 
 class BurguerBuilder extends Component {
   /* componentDidMount () {
