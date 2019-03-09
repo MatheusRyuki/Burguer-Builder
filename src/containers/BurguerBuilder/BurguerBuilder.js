3780d2f6f1c89b98a8ec12db31233666eacfd306
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-09 10:40:15 -0300

    Ajustando Checkout e os Dados de Contato

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -46,17 +46,7 @@ class BurguerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-      const queryParams = [];
-
-      for (let i in this.state.ingredients) {
-          queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-      }
-      queryParams.push('price=' + this.state.totalPrice);
-      const queryString = queryParams.join('&');
-      this.props.history.push({
-        pathname: '/checkout',
-        search: '?' + queryString
-      });
+    this.props.history.push('/checkout');
 };
 
   render () {
