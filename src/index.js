fdbcafbf2aba80545c09498d8988edcaeec2d50f
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-11 10:20:10 -0300

    Preparando a estrutura

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import reducer from './store/reducer/reducer';
 
 const store = createStore(reducer);
 
--- src/store/reducer/bugerBuilder.js
+++ src/store/reducer/bugerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionsType from './actions';
+import * as actionsType from '../actions/actions';
 
 const initialState = {
   ingredients: {
