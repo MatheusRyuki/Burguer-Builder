6d2eb19a35c05417e65af912b19247abb51e9cc4
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-12 15:05:18 -0300

    Adicionando ações da Order

---
+++ src/store/actions/order.js
@@ -0,0 +1,29 @@
+import * as actionTypes from './actions';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+        .then(response => {
+            dispatch(purchaseBurgerSuccess(response.data, orderData));
+        })
+        .catch(error => {
+          dispatch(purchaseBurgerFail(error));
+        });
+    };
+};
