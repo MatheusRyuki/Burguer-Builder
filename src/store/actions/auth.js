910a2ea7d0508ddfeb93b539e1056fd98936c916
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-17 09:51:02 -0300

    Pegando token do backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actions from './actions';
+import axios from '../../axios-orders';
 
 export const authStart = () => {
     return {
@@ -23,5 +24,17 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyATH5FKwUXe6WuL_jvwFnEHUSRwZz-TEzI", authData)
+            .then(response => {
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                dispatch(authFail(err));
+            });
     };
 };
