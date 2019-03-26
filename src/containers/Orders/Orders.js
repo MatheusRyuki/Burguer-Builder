ea779f8495e586ab0eb995cb58c16f0786c5fb5f
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-26 18:07:44 -0300

    Mostrando apenas os pedidos daquele usuário

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,7 @@ class Orders extends Component {
     loading: true
   }
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
   render () {
@@ -40,13 +40,14 @@ const mapStateToProps = state => {
   return {
     orders: state.order.orders,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: (token) => dispatch(actions.fetchOrders(token)),
+    onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId)),
   };
 };
 
