19a16f7fc8c3e249423ee039d62e5c4abae08222
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-26 00:05:24 -0300

    Guards no frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -15,25 +15,45 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-          <Route path='/auth' component={Auth} />
+
+    let routes = (
+      <Switch>
+      <Route path='/auth' component={Auth} />
+      <Route path="/" exact component={BurguerBuilder} />
+      <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
             <Route path='/orders' component={Orders} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurguerBuilder} />
+            <Route path="/" exact component={BurguerBuilder} />
+            <Redirect to="/" />
           </Switch>
+      );
+    }
+    return (
+      <div>
+        <Layout>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
 const mapActions = dispatch  => {
   return {
     onTryAutoSignUp: () => dispatch(actions.authCheckState()) 
   };
 };
-export default withRouter(connect(null, mapActions)(App));
+export default withRouter(connect(mapStateToProps, mapActions)(App));
