965ff66b1c117d24201939aa5708310c1674cdbc
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-04-05 21:37:59 -0300

    Testes prontos

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.test.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.test.js
@@ -2,22 +2,14 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItem from './NavigationItem';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 configure({
     adapter: new Adapter()
 });
 
 describe('Componente de NavigationItems', () => {
-    let wrapper;
-
-    beforeEach (() => {
-        wrapper = shallow(<NavigationItem />);
+    let wrapper = shallow(<NavigationItem  children="/" />);
+    it('Deve ter Props', () => {
+        expect(wrapper.props().children.props.children).toEqual('/');
     });
-
-    it('Deve ter NavLink', () => {
-        wrapper.setProps({to: '/'});
-        expect(wrapper.find(NavLink)).toHaveLength(1);
-    });
-
 });
